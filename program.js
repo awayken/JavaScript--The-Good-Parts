c7055c2b791e1ec5cde51639fead27f886e7b4a7
Miles Rausch <miles@milesrausch.com>
2011-11-22 01:33:46 -0600

    Method Invocation Pattern

--- program.js
+++ program.js
@@ -11,112 +11,27 @@ Function.prototype.method = function (name, func) {
 
 
 ////
-// Object Literal
+// Function Literal
 ////
-document.writeln('<br />Object Literal');
-var empty_object = {};
-
-var stooge = {
-	"first-name": "Jerome",
-	"last-name": "Howard"
+document.writeln('<br />Function Literal');
+var add = function (a, b) {
+	return a + b;
 };
 
-var flight = {
-	airline: "Oceanic",
-	number: 815,
-	departure: {
-		IATA: "SYD",
-		time: "2004-09-22 14:55",
-		city: "Sydney"
-	},
-	arrival: {
-		IATA: "LAX",
-		time: "2004-09-23 10:42",
-		city: "Los Angeles"
+
+
+
+////
+// Invocation
+////
+document.writeln('<br />Method Invocation Pattern');
+var myObject = {
+	value: 0,
+	increment: function (inc) {
+		this.value += typeof inc === 'number' ? inc : 1;
 	}
 };
-
-
-
-
-////
-// Prototype
-////
-document.writeln('<br />Prototype');
-if (typeof Object.create !== 'function') {
-	Object.create = function (o) {
-		var F = function () {};
-		F.prototype = o;
-		return new F();
-	};
-}
-var another_stooge = Object.create(stooge);
-     
-another_stooge['first-name'] = 'Harry';
-another_stooge['middle-name'] = 'Moses';
-another_stooge.nickname = 'Moe';
-
-stooge.profession = 'actor';
-document.writeln(another_stooge.profession);
-
-
-
-
-////
-// Enumeration
-////
-document.writeln('<br />Enumeration');
-var name;
-for (name in another_stooge) {
-	if (typeof another_stooge[name] !== 'function') {
-		document.writeln(name + ': ' + another_stooge[name]);
-	}
-}
-var i;
-var properties = [
-	'first-name',
-	'middle-name',
-	'last-name',
-	'profession'
-];
-for (i = 0; i < properties.length; i += 1) {
-	document.writeln(properties[i] + ': ' + another_stooge[properties[i]]);
-}
-
-
-
-
-////
-// Delete
-////
-document.writeln('<br />Delete');
-document.writeln(another_stooge.nickname);
-delete another_stooge.nickname;
-document.writeln(another_stooge.nickname);
-
-
-
-
-////
-// Global Abatement
-////
-document.writeln('<br />Global Abatement');
-var MYAPP = {};
-MYAPP.stooge = {
-	"first-name": "Joe",
-	"last-name": "Howard"
-};
-MYAPP.flight = {
-	airline: "Oceanic",
-	number: 815,
-	departure: {
-		IATA: "SYD",
-		time: "2004-09-22 14:55",
-		city: "Sydney"
-	},
-	arrival: {
-		IATA: "LAX",
-		time: "2004-09-23 10:42",
-		city: "Los Angeles"
-	}
-};
+myObject.increment( );
+document.writeln(myObject.value);
+myObject.increment(2);
+document.writeln(myObject.value);
